06f3294c61a7fcc8bdf7908bd56f5357fefd8541
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-10-31 10:38:05 +0000

    basics copied over

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# polar_bears_forms_django
-Using polar bear data to explore forms in Django
+# Parsing Polar Bear Data and Learning about Forms in Django
+This is a demonstrator app focusing on how you can use forms in a python Django web application using polar bear tracking data. This builds on a [previous demonstrator that pulled csv data into a web application](https://github.com/scharlau/polar_bears_django) to show the polar bear data. Go do that one first, so that you have everything in place. 
+
+Django follows a model-view-controller architecture so that you can put code in a 'good' place for reuse. However, in Django the names are different. Models are models, views in Django are controllers, and it uses templates as views. Remember this as we go forward and it will help to keep things clearer.
+
+The purpose of this exercise is to add forms to the application, which currently has none. That means we can only change data by deleting data in the database, and then editing the csv, and reloading it. That's a fine to start, but not really how we'd go about using this to add new bears, and sightings, or editing data.
+
+The goal of 'deliberate practice' is to think about how you'd solve this challenge, and to work at developing code to make this work. There is no single 'correct' version of this code. The purpose of the exercise it become familiar with different ways of making the application work. You should explore how this simple application is done in python with flask and sqlite3 so that you understand how the components work together to show up in the views you see in the browser.
+
+Under 'deliberate practice' we offer up the challenge, then think about options for developing a solution, and code for 12 minutes. After that we pause to discuss how people are approaching the problem, and what they're trying to do. This should be repeated three times and then wrapped up with time for people to express what they found most useful during the session. This should take an hour.
+
+
+
+## Doing the work
+Now that the basics are working, we can see what else is possible in this application.
+
+Round one should have you adding in a basic form to edit a 'bear'. 
+Round two should be adding forms to edit 'sightings'.
+Round three should be exploring what else might be possible, even if only to total up some values of the bear attributes to display on a page.
---
+++ bears/management/commands/parse_csv.py
@@ -0,0 +1,81 @@
+import csv
+import os
+from pathlib import Path
+from django.db import models
+from django.core.management.base import BaseCommand, CommandError
+
+from bears.models import Bear, Sighting
+
+'''
+We use the command tools so that we gain access to our models and database connections
+https://docs.djangoproject.com/en/3.1/howto/custom-management-commands/ 
+'''
+
+class Command(BaseCommand):
+    help = 'Load data from csv'
+
+    def handle(self, *args, **options):
+
+        # drop the data from the table so that if we rerun the file, we don't repeat values
+        Sighting.objects.all().delete()
+        Bear.objects.all().delete()
+        print("tables dropped successfully")
+
+        # open the file to read it into the database
+        base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        with open(str(base_dir) + '/bears/PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartag_deployments_2009-2011.csv', newline='') as f:
+            reader = csv.reader(f, delimiter=",")
+            next(reader) # skip the header line
+            for row in reader:
+                print(row)
+
+                bear = Bear.objects.create(
+                bearID = int(row[0]),
+                pTT_ID = int(row[1]),
+                capture_lat = float(row[6]),
+                capture_long = float(row[7]),
+                sex = row[9],
+                age_class = row[10],
+                ear_applied = row[11],
+                )
+                bear.save()
+
+        base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        with open(str(base_dir) + '/bears/PolarBear_Telemetry_southernBeaufortSea_2009_2011/USGS_WC_eartags_output_files_2009-2011-Status.csv', newline='') as f:
+            reader = csv.reader(f, delimiter=",")
+            next(reader) # skip the header line
+            for row in reader:
+                print(row)
+                
+                # sanity checking for empty cells - this clears up most missing data to parse the file
+                # the file will still fall over at some point, but you get enough to show a good example
+                temp_temp = 0
+                temp_bear_id = 0
+                if row[4] is not '':
+                    if row[9] is not '':
+                        temp_temp = row[9]
+                    else:
+                        temp_temp = 0 # this works but not if we want to be correct
+
+                    bear_temp = row[0]
+                    print(bear_temp)
+                    bear = Bear.objects.filter(bearID = bear_temp).first()
+                    print(bear.id)
+
+                    sighting = Sighting.objects.create(
+                    deploy_id = int(row[0]),
+                    bear_id = bear,
+                    recieved = row[2],
+                    latitude = float(row[4]),
+                    longitude = float(row[5]),
+                    temperature = float(temp_temp),
+                    )
+                    sighting.save()
+                    print(sighting.id)
+
+                else: # skip the row
+                    next(reader)
+
+        print("data parsed successfully")
+
+        

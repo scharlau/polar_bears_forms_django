3b84927aea0cb49cefc6c0299c25defa1a94c268
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-01 08:44:13 +0000

    started form basics

--- bears/urls.py
+++ bears/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
         path('', views.bear_list, name='bear_list'),
         path('females', views.females, name='females'),
         path('bear/<int:id>/', views.bear_detail, name= 'bear_detail'),
+        path('bear_new/', views.bear_new, name='bear_new'),
+        path('bear/<int:id>/edit/', views.bear_edit, name='bear_edit'),
+        path('bear/<int:id>/delete/', views.bear_delete, name='bear_delete'),
         ]
--- bears/views.py
+++ bears/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Bear
+from .forms import BearForm
+
+def bear_new(request):
+    if request.method=="POST":
+        form = BearForm(request.POST)
+        if form.is_valid():
+            bear = form.save(commit=False) # don't save yet, as want to add created_date
+            bear.created_date = timezone.now()
+            bear.save()
+            return redirect('bear_detail', id=bear.id) # use bear.id as we already have instance
+    else:
+        form = BearForm()
+    return render(request, 'bears/bear_edit.html', {'form': form}) #folder/file_name under 'templates' folder
+
+def bear_edit(request, id):
+    bear = get_object_or_404(Bear, id=id)
+    if request.method=="POST":
+        form = BearForm(request.POST, instance=bear)
+        if form.is_valid():
+            bear = form.save(commit=False)
+            bear.created_date = timezone.now()
+            bear.save()
+            return redirect('bear_detail', id=bear.id)
+    else:
+        form = BearForm(instance=bear)
+    return render(request, 'bears/bear_edit.html', {'form': form, 'bear': bear})
+
+def bear_delete(request, id):
+    bear = get_object_or_404(Bear, id=id)
+    deleted = request.session.get('deleted', 'empty')
+    request.session['deleted'] = bear.id
+    bear.delete()
+    return redirect('bear_list' )
 
 def bear_list(request):
     bears = Bear.objects.all()
